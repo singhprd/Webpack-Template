5ff8a1f53fddf3ee45d44e1e7691de2b30c9f124
Peter Singh <singh.prd@gmail.com>
2016-04-06 13:30:16 +0100

    initial

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+
+app.use(express.static('client/build'));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
+app.get('/helloworld', function(req, res) {
+  res.send('hello world');
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
